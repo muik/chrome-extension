9f026e2b081fb5f23889100d95a806d821d10ac6
Ed Jeon <muikor@gmail.com>
2018-08-08 23:05:40 +0900

    Fix to add cart

--- add_cart.js
+++ add_cart.js
@@ -1,3 +1,7 @@
-setTimeout(function() {
-  $('div.deal_topinfo button[data-type="addToCart"]')[0].click();
-}, 500);
+var dom = $('div.item.total strong[data-role="resultPrice"]');
+dom.on("DOMSubtreeModified", function(e) {
+  if (this.textContent.length > 1) {
+    dom.off('DOMSubtreeModified');
+    $('div.deal_topinfo button[data-type="addToCart"]')[0].click();
+  }
+});
